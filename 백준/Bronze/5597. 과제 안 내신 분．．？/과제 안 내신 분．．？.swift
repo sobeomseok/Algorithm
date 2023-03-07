5125983152ad910163dd1d10c3723438de7152d7
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-07 11:13:44 +0900

    Update 과제 안 내신 분．．？.swift

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
@@ -22,3 +22,22 @@ for _ in 0..<input {
 let arrDuplicate: Set<Int> = Set(arr)
 
 print(arrDuplicate.count)
+
+let input = readLine()!.split(separator: " ").map {Int($0)!}
+let N = input[0]
+let M = input[1]
+var arr = [Int](1...N)
+
+for _ in 0..<M {
+    let input = readLine()!.split(separator: " ").map {Int($0)!}
+    var i = input[0] - 1
+    var j = input[1] - 1
+    
+    while i < j {
+        arr.swapAt(i, j)
+        i += 1
+        j -= 1
+    }
+}
+
+print(arr.map {String($0)}.joined(separator: " "))
