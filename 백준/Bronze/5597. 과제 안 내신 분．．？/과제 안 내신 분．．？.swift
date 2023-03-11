f76a06334e1a6dee3a01bd5f1fa61ea65d61fb0b
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-11 20:24:02 +0900

    Update 과제 안 내신 분．．？.swift

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.swift
@@ -10,34 +10,3 @@ for i in 1...30 {
         print(i)
     }
 }
-
-let division: Int = 42
-let input: Int = 10
-var arr = [Int]()
-
-for _ in 0..<input {
-    arr.append(Int(readLine()!)! % division)
-}
-
-let arrDuplicate: Set<Int> = Set(arr)
-
-print(arrDuplicate.count)
-
-let input = readLine()!.split(separator: " ").map {Int($0)!}
-let N = input[0]
-let M = input[1]
-var arr = [Int](1...N)
-
-for _ in 0..<M {
-    let input = readLine()!.split(separator: " ").map {Int($0)!}
-    var i = input[0]
-    var j = input[1]
-    
-    while i < j {
-        arr.swapAt(i - 1, j - 1)
-        i += 1
-        j -= 1
-    }
-}
-
-print(arr.map {String($0)}.joined(separator: " "))
