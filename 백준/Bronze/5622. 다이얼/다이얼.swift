f23af7e6bc09c736e4b5b6a1e15a0eb26c3770b6
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-16 15:53:12 +0900

    Update 다이얼.swift

--- 백준/Bronze/5622. 다이얼/다이얼.swift
+++ 백준/Bronze/5622. 다이얼/다이얼.swift
@@ -1,4 +1,19 @@
-var times = [3, 3, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 10, 10, 10, 10]
+var times = [Int]()
+var number = 3 
+
+// 배열에 각 알파벳 번호 넣기
+for i in 0..<8 {  // 다이얼이 2~9번 총 8자리
+    for _ in 0..<3 {  // 한 번호에 3개 있음
+        times.append(number)
+    }
+    
+    if i == 5 || i == 7 {  // 한 번호에 4개 있는 경우
+        times.append(number)
+    }
+    
+    number += 1
+}
+
 let input = readLine()!.map {times[Int($0.asciiValue!) - 65]}.reduce(0, +)
 
 print(input)
