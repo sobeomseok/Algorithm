728fc03bbb5c53618da0bad6f8fa37c8f679a42b
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-17 14:23:59 +0900

    Update 별 찍기 － 7.swift

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.swift
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.swift
@@ -17,7 +17,7 @@ for i in 0..<input - 1 {
         print(" ", terminator: "")
     }
     
-    for _ in stride(from: (input - 1) * 2 - i * 2 - 1, to: 0, by: -1) {
+    for _ in stride(from: (input - 1 - i) * 2 - 1, to: 0, by: -1) {
         print("*", terminator: "")
     }
     
