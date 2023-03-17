083a4ddfb47301e2610f38cc7cf8c3ee250f601a
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-17 13:58:58 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 28 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.swift
@@ -0,0 +1,25 @@
+let input = Int(readLine()!)!
+
+for i in 0..<input {
+    for _ in stride(from: input - i - 1, to: 0, by: -1) {
+        print(" ", terminator: "")
+    }
+    
+    for _ in 0..<i * 2 + 1 {
+        print("*", terminator: "")
+    }
+    
+    print()
+}
+
+for i in 0..<input - 1 {
+    for _ in 0...i {
+        print(" ", terminator: "")
+    }
+    
+    for _ in stride(from: (input - 1) * 2 - i * 2 - 1, to: 0, by: -1) {
+        print("*", terminator: "")
+    }
+    
+    print()
+}
