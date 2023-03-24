921a303521437d726f6c4a42dca3a71be1820584
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-24 15:36:52 +0900

    [Silver II] Title: 스택 수열, Time: 88 ms, Memory: 86012 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+let n = Int(readLine()!)!
+
+var count = 1
+var stack: [Int] = []
+var answer: [String] = []
+
+
+for _ in 0..<n {
+    
+    let num = Int(readLine()!)!
+    
+    while count <= num {
+        stack.append(count)
+        answer.append("+")
+        count += 1
+    }
+    
+    if stack.last == num {
+        stack.popLast()
+        answer.append("-")
+    } else {
+        print("NO")
+        exit(0)
+    }
+    
+}
+
+print(answer.joined(separator: "\n"))
