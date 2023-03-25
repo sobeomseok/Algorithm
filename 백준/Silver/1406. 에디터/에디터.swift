dcaa04b71464798a4b1a1bb44db0f53f1a41adb1
Bas <67938946+sobeomseok@users.noreply.github.com>
2023-03-25 16:39:10 +0900

    [Silver II] Title: 에디터, Time: 288 ms, Memory: 70620 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.swift
@@ -0,0 +1,33 @@
+var leftInput = readLine()!
+let count = Int(readLine()!)!
+var rightInput = ""
+
+for _ in 0..<count {
+    let input = readLine()!
+    
+    if input == "L" {
+        if !leftInput.isEmpty {
+            rightInput.append(leftInput.popLast()!)
+        }
+    } else if input == "D" {
+        if !rightInput.isEmpty {
+            leftInput.append(rightInput.popLast()!)
+        }
+    } else if input == "B" {
+        if !leftInput.isEmpty {
+            leftInput.removeLast()
+        }
+    } else {
+        leftInput.append(input.last!)
+    }
+}
+
+var tmp = ""
+
+for _ in 0..<rightInput.count {
+    tmp.append(rightInput.popLast()!)
+}
+
+rightInput = tmp
+
+print(leftInput + rightInput)
